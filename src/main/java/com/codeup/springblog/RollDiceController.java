a2c9397e01af7ff3156f91554268ffe1e12f306a
daniel-browning <daniel.d.browning@gmail.com>
2018-10-30 10:14:13 -0500

    adding roll-dice exercise;

---
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -0,0 +1,28 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{userGuess}")
+    public String userGuess(@PathVariable int userGuess, Model viewModel) {
+        Random random = new Random();
+        int diceNumber = random.nextInt((6 - 1) + 1) + 1; //(random() * ((max - min) + 1)) + min
+        viewModel.addAttribute("userGuess", userGuess);
+        viewModel.addAttribute("diceNumber", diceNumber);
+
+        return "dice-results";
+    }
+
+}
